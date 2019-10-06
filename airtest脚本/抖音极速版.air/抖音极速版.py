7b2ddc92c58f612391c832c5fe0e7dc10f7482a3
tony <826537273@qq.com>
2019-10-06 18:50:47 +0800

    add

---
+++ airtest脚本/抖音极速版.air/抖音极速版.py
@@ -0,0 +1,59 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+
+from airtest.core.api import *
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+import datetime
+import random
+
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+
+auto_setup(__file__)
+devs = device()
+print(devs.list_app(third_only=True))
+#自定义滑动事件
+def getSize():
+    s=poco.get_screen_size()
+    return (s[0], s[1])
+#屏幕向上滑动
+def swipeUp(t,percent):
+    l = getSize()
+    x1 = int(l[0] * 0.5)+ random.randint(0,20) #x坐标
+    y1 = int(l[1] * 0.8)  #起始y坐标
+    y2 = int(l[1] * percent)   #终点y坐标
+
+    swipe((x1, y1), (x1, y2),t+random.randint(0,20))
+    
+# =============================================================
+# 抖音极速版app
+# 先关闭视频app
+stop_app("com.ss.android.ugc.aweme.lite")
+# 开始阅读趣头条小视频        
+start_app("com.ss.android.ugc.aweme.lite",activity=None)
+# 进入小视频接口
+# 等待15秒启动加载
+sleep(20)
+
+file = r'D:\抖音极速版.log'
+f = open(file, 'w+')
+start = datetime.datetime.now()
+
+# 查看次数
+watch_num = 1
+while(True):
+    sleep(10 +random.randint(0,8))
+    swipeUp(1000,0.1)
+
+    # 根据时间判断 两个小时结束
+    cur = datetime.datetime.now()
+    timeStyle=cur.strftime("%Y-%m-%d %H:%M:%S")
+    strlog = '查看一个抖音极速版小视频，已看%d个' % watch_num
+    strlog =strlog +timeStyle+'\n'
+    f.write(strlog)
+    print(strlog)
+    watch_num += 1
+    if((cur-start).seconds >= 3600*2):
+        break
+
+f.close()
+stop_app("com.ss.android.ugc.aweme.lite")
